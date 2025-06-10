cfa65cff4debc1d2dedb2557e8d4ad90ac03981f
g196one <g196one@gmail.com>
2025-06-10 10:17:51 +0900

    예제파일 업로드

---
+++ Ex_250312_mod/src/Main.java
@@ -0,0 +1,13 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        //짝수, 홀수 구분 프로그램
+
+        int x = 5;
+        System.out.println(x % 2 == 1);
+        int y = 6;
+        System.out.println(y % 2 == 1);
+
+    }
+}
